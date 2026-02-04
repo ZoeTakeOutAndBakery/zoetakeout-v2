8d110bdbe0d9cfb746d672ba43b95469ef2d8ae4
ZoeTakeOutAndBakery <zoetakeoutandbakery@gmail.com>
2026-02-04 15:22:09 -0500

    Base

---
+++ zoe-bakes-bites/sanity/schemaTypes/index.ts
@@ -0,0 +1,15 @@
+import siteSettings from "./siteSettings";
+import hours from "./hours";
+import menuCategory from "./menuCategory";
+import menuItem from "./menuItem";
+import testimonial from "./testimonial";
+import deliveryLink from "./deliveryLink";
+
+export const schemaTypes = [
+  siteSettings,
+  hours,
+  menuCategory,
+  menuItem,
+  testimonial,
+  deliveryLink,
+];
---
+++ zoe-bakes-bites/sanity/schemaTypes/menuItem.ts
@@ -0,0 +1,18 @@
+import { defineField, defineType } from "sanity";
+
+export default defineType({
+  name: "menuItem",
+  title: "Menu Item",
+  type: "document",
+  fields: [
+    defineField({ name: "name", title: "Name", type: "string" }),
+    defineField({ name: "description", title: "Description", type: "text" }),
+    defineField({ name: "price", title: "Price", type: "string" }),
+    defineField({
+      name: "tags",
+      title: "Tags",
+      type: "array",
+      of: [{ type: "string" }],
+    }),
+  ],
+});
